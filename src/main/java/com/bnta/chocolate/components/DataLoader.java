25a28b131d8ccc985ae857d8c33a11d75ab3d821
rimmderes <rimmderes@gmail.com>
2022-12-07 16:07:08 +0000

    Added date Loader Rimm

---
+++ src/main/java/com/bnta/chocolate/components/DataLoader.java
@@ -0,0 +1,30 @@
+package com.bnta.chocolate.components;
+
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    @Autowired
+    EstateRepository estateRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+
+
+    }
+
+
+}
